bf45e40224aa0a182a3e6ca33e65ae099e04cc65
Laura Abend <laura.l.abend@gmail.com>
2016-10-15 22:11:31 -0500

    add database functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,15 +1,47 @@
 console.log('Client is running');
-
 $(function () {
 
-  var testObj = { name: 'Laura', age: '33' };
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    console.log('this:', this);
 
-  $.ajax({
-    type: 'POST',
-    url: '/app',
-    data: testObj,
-    success: function (testObj) {
-      console.log('testObj:', testObj);
-    },
+    var taskObj = {};
+    var task = $('input').val();
+    console.log('input:', task);
+
+    var $input = $('#input');
+    $input.empty();
+
+    taskObj.task = task;
+    taskObj.complete = 'no';
+    console.log('taskObj:', taskObj);
+
+    $.ajax({
+      type: 'POST',
+      url: '/app',
+      data: taskObj,
+      success: function (response) {
+        console.log('post req response:', response);
+        // var task = taskObj.task;
+        // console.log('post req task:', task);
+      },
+    });
   });
+
+  // $.ajax({
+  //   type: 'GET',
+  //   url: '/app',
+  //   success: displayTask,
+  // });
+
+  // test to make sure client and router are playing nice
+  // var testObj = { name: 'Laura', age: '33' };
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/app',
+  //   data: testObj,
+  //   success: function (testObj) {
+  //     console.log('testObj:', testObj);
+  //   },
+  // });
 });
--- routes/app.js
+++ routes/app.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
-router.post('/', function (req, res) {
+var config = {
+  database: 'to_do',
+};
+
+var pool = new pg.Pool(config);
+
+router.get('/', function (req, res) {
   console.log('req.body:', req.body);
   res.sendStatus(200);
+});
+
+router.post('/', function (req, res) {
+  pool.connect(function (err, client, done) {
+    if (err) {
+      console.log('Error connecting to the DB', err);
+      res.sendStatus(500);
+      done();
+      return;
+    }
+
+    client.query('INSERT INTO tasks (task, complete) VALUES ($1, $2) returning *;',
+    [req.body.task, req.body.complete],
+     function (err, result) {
+      done();
+      if (err) {
+        console.log('Error querying the DB', err);
+        res.sendStatus(500);
+        return;
+      }
+
+      console.log('Got rows from the DB:', result.rows);
+      res.send(result.rows);
+    });
+  });
 });
 
 module.exports = router;

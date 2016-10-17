59b6013ab1ff3c601997a37344b281e719439394
Laura Abend <laura.l.abend@gmail.com>
2016-10-17 06:45:55 -0500

    update database.sql with one less step

--- database.sql
+++ database.sql
@@ -4,9 +4,6 @@ CREATE DATABASE to_do;
 -- 2. Create table
 CREATE TABLE tasks (
 id SERIAL PRIMARY KEY,
-task varchar(80) NOT NULL
+task varchar(80) NOT NULL,
+complete varchar (5)
 )
-
--- 3. Alter table to add a column (it wasn't allowing me to add it on Step 2)
-ALTER TABLE tasks
-ADD complete varchar(5);
